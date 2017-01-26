c040e17f624190f6058f8bcc84712432b165f292
Simon Su <simonsu.mail@gmail.com>
2017-01-26 16:29:05 +0800

    add eval for fn data

--- client.js
+++ client.js
@@ -9,7 +9,11 @@ var data = {
     token: '3eea71e6f263c13de696e999491118033e62f545133e4186b6b4c14a90b3031e9b332078a6fe1c655d25a917e06eb43b' 
   };
 
-var socket = require('socket.io-client')(socket_server, {query:data});
+var socket = require('socket.io-client')(socket_server, {
+	query:data, 
+	timeout: (process.env.TIMEOUT || 30000), 
+	reconnectionDelay: (process.env.RECONNECT_DELAY || 1000)
+});
 
 socket.on('connect', function(){
   console.log('on connect...', (socket.id));
@@ -19,9 +23,22 @@ socket.on('message', function(data){
   console.log('[%s][%s] got event...', socket.id, new Date().getTime(), data);
 });
 
+socket.on('cmd', function(data){
+  console.log('[%s][%s] got cmd...', socket.id, new Date().getTime(), data);
+	data = JSON.parse(data);
+	eval(data.cmd);
+	data.cmd = fn;
+  if(data && data['cmd']) 
+		data.cmd(socket.id);
+});
+
+
+
 if(process.env.TALK) {
 	setInterval(function(){
-	  socket.emit('message', genTalk());
+    var msg = genTalk();
+    console.log('emit msg:', msg);
+	  socket.emit('message', msg);
 	}, process.env.INTERVAL || 3000);
 }
 
--- server.js
+++ server.js
@@ -1,45 +1,60 @@
 var server = require('http').createServer();
 var io = require('socket.io')(server);
-var monitor = require('./monitor');
 
-if(process.env.REDIS_SERVER) {
-	//For clustering socket server
-	var redis = require('socket.io-redis');
-	//You can run the redis server: docker run --name myredis -p 6379:6379 -d redis
-	io.adapter(redis({ host: process.env.REDIS_SERVER || 'localhost', port: 6379 }));
+if (process.env.COUCHDB)
+    var monitor = require('./monitor');
+
+if (process.env.REDIS_SERVER) {
+    //For clustering socket server
+    var redis = require('socket.io-redis');
+    //You can run the redis server: docker run --name myredis -p 6379:6379 -d redis
+    io.adapter(redis({ host: process.env.REDIS_SERVER || 'localhost', port: 6379 }));
 }
 
-var sio = io.on('connection', function(client){
-  client.on('message', function(data){
-    console.log('Got message:', data);
-  });
+var sio = io.on('connection', function(client) {
+    client.on('message', function(data) {
+        console.log('Got message:', data);
+    });
 
-	if(process.env.TALK) {
-    setInterval(function(){
-			client.emit('message', genTalk());
-		}, process.env.INTERVAL || 3000);
-	}
-  client.on('disconnect', function(){
-		console.log('client:%s disconnect', client.id);
-  });
+    if (process.env.TALK) {
+        setInterval(function() {
+            client.emit('message', genTalk());
+        }, process.env.INTERVAL || 3000);
+    }
+    client.on('disconnect', function() {
+        console.log('client:%s disconnect', client.id);
+    });
 });
+
+var fn = function(x) {
+    console.log('client:', x);
+};
 
 //if(process.env.BROADCAST)
 setInterval(function() {
-	sio.emit('message','sio emit... ' + new Date().getTime());
-	console.log('sio:', Object.keys(sio.sockets).length);
-  monitor.setSockets(Object.keys(sio.sockets).length);
+    sio.emit('message', 'sio emit... ' + new Date().getTime());
+    var d = {
+        aa: '12341234',
+        cmd: "var fn = function(x) {console.log('>>>>>>>>>>', x);}"
+    };
+    console.log('--->', JSON.stringify(d));
+    sio.emit('cmd', JSON.stringify(d));
+    console.log('sio:', Object.keys(sio.sockets).length);
+    if (process.env.COUCHDB)
+        monitor.setSockets(Object.keys(sio.sockets).length);
 }, process.env.INTERVAL || 3000);
 
 server.listen(process.env.PORT || 3000);
 
 var talk = ['hello!', 'nice to meet you....', 'good job', 'gg...@@',
-  'who are you?', 'good morning', ':D', ':(', 'give up...'];
+    'who are you?', 'good morning', ':D', ':(', 'give up...'
+];
+
 function genTalk() {
-  var idx = Math.round(Math.random()*1000) % talk.length;
-  return {
-    type: 'text',
-    content: talk[idx],
-    ts: new Date().getTime()
-  };
+    var idx = Math.round(Math.random() * 1000) % talk.length;
+    return {
+        type: 'text',
+        content: talk[idx],
+        ts: new Date().getTime()
+    };
 }
